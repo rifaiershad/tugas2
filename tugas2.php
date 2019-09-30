778d8b86e79fbb1530d21714958e939bf90f1b41
rifaiershad <55640054+rifaiershad@users.noreply.github.com>
2019-09-30 19:03:13 +0700

    Add files via upload

---
+++ tugas2.php
@@ -0,0 +1,9 @@
+<?php
+    for($no=1;$no<=60;$no++)
+        {
+            if($no % 2 == 0)
+                {echo "$no = Bilangan Genap<br>";}
+            else
+                {echo "$no = Bilangan Ganjil<br>";}
+        }
+?>
